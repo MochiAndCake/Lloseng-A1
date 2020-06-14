05523eb915fe6ee9899d25bc73d8184724d1a92a
Ann JS <36861363+MochiAndCake@users.noreply.github.com>
2020-06-14 17:37:39 -0400

    Add existing file

--- code/simplechat1/ServerConsole.java
+++ code/simplechat1/ServerConsole.java
@@ -3,7 +3,19 @@ import common.*;
 
 public class ServerConsole implements ChatIF {
 
+  final public static int DEFAULT_PORT = 5555;
+
   EchoServer server;
+
+  public ServerConsole(int port) {
+    server = new EchoServer(port);
+
+    try {
+      server.listen(); //Start listening for connections
+    } catch (Exception ex) {
+      System.out.println("ERROR - Could not listen for clients!");
+    }
+  }
 
   public void obtainInput() {
     try {
@@ -18,5 +30,29 @@ public class ServerConsole implements ChatIF {
       System.out.println
         ("Unexpected error while reading from console!");
     }
+  }
+
+  public void display (String message) {
+    System.out.println("SERVER MSG> " + message);
+  }
+
+  /**
+   * This method is responsible for the creation of
+   * the server instance (there is no UI in this phase).
+   *
+   * @param args[0] The port number to listen on.  Defaults to 5555
+   *          if no argument is entered.
+   */
+  public static void main(String[] args) {
+    int port = 0; //Port to listen on
+
+    try {
+      port = Integer.parseInt(args[0]); //Get port from command line
+    } catch(Throwable t) {
+      port = DEFAULT_PORT; //Set port to 5555
+    }
+
+    ServerConsole console = new ServerConsole(port);
+    console.obtainInput();
   }
 }
